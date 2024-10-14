b8ce88c47c25dc0384c83daf7f0f506dc49f8db1
Dennis <141487520+Dionysos599@users.noreply.github.com>
2024-10-14 14:57:56 -0700

    Update Editor.tsx

--- hw2/src/Editor.tsx
+++ hw2/src/Editor.tsx
@@ -3,16 +3,9 @@ import { Location, Marker, COLORS } from './marker';
 import { BUILDINGS } from './buildings';
 
 type EditorProps = {
-    /** The marker that the user wants to edit. */
     marker: Marker;
-
-    /** If provided, let the user move to this location. */
     moveTo?: Location;
-
-    /** Callback to invoke when the user wants to cancel editing. */
     onCancelClick: () => void;
-
-    /** Callback to invoke when the user wants to save the edit. */
     onSaveClick: (name: string, color: string, loc: Location) => void;
 };
 
@@ -24,11 +17,9 @@ type EditorState = {
     allowCustomMoveTo: boolean;
 };
 
-/** Component that allows the user to edit a marker. */
 export class Editor extends Component<EditorProps, EditorState> {
     constructor(props: EditorProps) {
         super(props);
-
         this.state = {
             name: props.marker.name,
             color: props.marker.color,
@@ -57,150 +48,72 @@ export class Editor extends Component<EditorProps, EditorState> {
 
     getSelectedBuildingInfo = (): { name: string; location: Location } => {
         const selectedBuilding = BUILDINGS.find(b => b.shortName === this.state.listedMoveTo);
-        if (selectedBuilding) {
-            return {
-                name: selectedBuilding.longName,
-                location: selectedBuilding.location,
-            };
-        }
-        // no building selected, return the current marker.
-        return {
-            name: this.state.name,
-            location: this.props.marker.location,
-        };
+        return selectedBuilding ? { name: selectedBuilding.longName, location: selectedBuilding.location}
+                                : { name: this.state.name, location: this.props.marker.location};
     };
 
     getCustomLoc = (): Location => {
-        if (this.props.moveTo && this.state.allowCustomMoveTo) {
-            return this.props.moveTo;
+        return this.props.moveTo && this.state.allowCustomMoveTo ? this.props.moveTo : this.props.marker.location;
+    };
+
+    handleSaveClick = () => {
+        const { name, color } = this.state;
+        if (!name) {
+            alert('Please enter a name for the building.');
+            return;
         }
-        return this.props.marker.location;
+        const location = this.props.moveTo ? this.getCustomLoc() : this.getSelectedBuildingInfo().location;
+        this.props.onSaveClick(name, color, location);
     };
 
     render = (): JSX.Element => {
+        const { name, color, listedMoveTo, filter, allowCustomMoveTo } = this.state;
         const filteredBuildings = this.filteredBuildings();
-
-        if (this.props.moveTo) {
-            return (
-                <div>
-                    <p>
-                        Name:
-                        <span style={{marginLeft: '5px'}}></span>
-                        <input
-                            type="text"
-                            value={this.state.name}
-                            onChange={(e) => this.setState({name: e.target.value})}
-                        />
-                    </p>
-                    <p>
-                        Color:
-                        <span style={{marginLeft: '5px'}}></span>
-                        <select
-                            value={this.state.color}
-                            onChange={(e) => this.setState({color: e.target.value})}
-                        >
-                            {COLORS.map((color) => (
-                                <option key={color} value={color}> {color} </option>
-                            ))}
-                        </select>
-                    </p>
-                    <p>
-                        <input
-                            type="checkbox"
-                            checked={this.state.allowCustomMoveTo}
-                            onChange={(e) => this.setState({ allowCustomMoveTo: e.target.checked })}
-                        />
-                        move to new location (gray)
-                    </p>
-                    <button
-                        onClick={() => {
-                            if (!this.state.name) {
-                                alert('Please enter a name for the building.');
-                                return;
-                            }
-                            this.props.onSaveClick(
-                                this.state.name,
-                                this.state.color,
-                                this.getCustomLoc()
-                            )
-                        }}
-                    >
-                        Save
-                    </button>
-                    <span style={{marginLeft: '5px'}}></span>
-                    <button onClick={this.props.onCancelClick}>
-                        Cancel
-                    </button>
-                </div>
-            );
-        }
 
         return (
             <div>
                 <p>
                     Name:
-                    <span style={{marginLeft: '5px'}}></span>
-                    <input
-                        type="text"
-                        value={this.state.name}
-                        onChange={(e) => this.setState({name: e.target.value})}
-                    />
-                    <span style={{marginLeft: '15px'}}></span>
-
-                    Move To:
-                    <span style={{marginLeft: '5px'}}></span>
-                    <select
-                        onChange={(e) => this.setState({listedMoveTo: e.target.value})}
-                        value={this.state.listedMoveTo}
-                    >
-                        <option value="">Select a building</option>
-                        {filteredBuildings.map((building) => (
-                            <option key={building.shortName} value={building.shortName}>
-                                {building.longName}
-                            </option>
-                        ))}
-                    </select>
+                    <span style={{ marginLeft: '5px' }} />
+                    <input type="text" value={name} onChange={(e) => this.setState({ name: e.target.value })}/>
+                    {this.props.moveTo ? undefined : (<>
+                        <span style={{ marginLeft: '15px' }} />
+                        Move To:
+                        <span style={{ marginLeft: '5px' }} />
+                        <select value={listedMoveTo} onChange={(e) => this.setState({ listedMoveTo: e.target.value })}>
+                            <option value="">Select a building</option>
+                            {filteredBuildings.map(building => (
+                                <option key={building.shortName} value={building.shortName}> {building.longName} </option>
+                            ))}
+                        </select>
+                    </> )}
                 </p>
                 <p>
                     Color:
-                    <span style={{marginLeft: '5px'}}></span>
-                    <select
-                        value={this.state.color}
-                        onChange={(e) => this.setState({color: e.target.value})}
-                    >
-                        {COLORS.map((color) => (
-                            <option key={color} value={color}> {color} </option>
-                        ))}
+                    <span style={{ marginLeft: '5px' }} />
+                    <select value={color} onChange={(e) => this.setState({ color: e.target.value })}>
+                        {COLORS.map(c => (<option key={c} value={c}> {c} </option>))}
                     </select>
-                    <span style={{marginLeft: '100px'}}></span>
-
-                    Filter:
-                    <span style={{marginLeft: '5px'}}></span>
-                    <input
-                        type="text"
-                        value={this.state.filter}
-                        onChange={(e) => this.setState({filter: e.target.value})}
-                        placeholder="Search buildings..."
-                    />
-                    <span style={{marginLeft: '10px'}}></span>
-                    (show only buildings including this text)
+                    {this.props.moveTo ? (
+                        <p>
+                            <input type="checkbox" checked={allowCustomMoveTo} onChange={(e) => this.setState({ allowCustomMoveTo: e.target.checked })}/>
+                            move to new location (gray)
+                        </p>
+                    ) : (
+                        <>
+                            <span style={{ marginLeft: '100px' }} />
+                            Filter: <span style={{ marginLeft: '5px' }} />
+                            <input type="text" value={filter} onChange={(e) => this.setState({ filter: e.target.value })}
+                                placeholder="Search buildings..."
+                            />
+                            <span style={{ marginLeft: '10px' }} />
+                            (show only buildings including this text)
+                        </>
+                    )}
                 </p>
-                <button
-                    onClick={() => {
-                        const {name, location} = this.getSelectedBuildingInfo();
-                        if (!name) {
-                            alert('Please enter a name for the building.');
-                            return;
-                        }
-                        this.props.onSaveClick(name, this.state.color, location);
-                    }}
-                >
-                    Save
-                </button>
-                <span style={{marginLeft: '5px'}}></span>
-                <button onClick={this.props.onCancelClick}>
-                    Cancel
-                </button>
+                <button onClick={this.handleSaveClick}>Save</button>
+                <span style={{ marginLeft: '5px' }} />
+                <button onClick={this.props.onCancelClick}>Cancel</button>
             </div>
         );
     };
